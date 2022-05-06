2d1f503627a044e16f203c9cba3f9bb6b88e6447
Anton M <antmat@ttu.ee>
2022-05-06 15:16:56 +0300

    init commit

---
+++ src/main/java/com/example/vidconvfer/VidconferApplication.java
@@ -0,0 +1,13 @@
+package com.example.vidconvfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidconferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidconferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vidconvfer/VidconferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vidconvfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidconferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
