5ce8b1013a1d8eab586cc04a5ef709c911408e43
Anton M <antmat@ttu.ee>
2022-05-10 08:34:55 +0300

    recording test

--- src/main/resources/static/vidconfer.js
+++ src/main/resources/static/vidconfer.js
@@ -2,14 +2,60 @@ let peerConnection;
 let mediaRecorder;
 let recordedBlobs;
 
-const codecPreferences = document.querySelector('#codecPreferences');
-
 const errorMsgElement = document.querySelector('span#errorMsg');
-const recordedVideo = document.querySelector('video#recorded');
+const recordedRemote = document.querySelector('video#remoteRecord');
+const recordedLocal = document.querySelector('video#localRecord');
 const recordButton = document.querySelector('button#record');
 
 const exitBtn = document.getElementById('exit');
+
+const mime = 'video/mp4;codecs=h264,aac';
+
 exitBtn.addEventListener('click', exit);
+
+recordButton.addEventListener('click', () => {
+    if (recordButton.textContent === 'Start Recording') {
+        startRecording();
+    } else {
+        stopRecording();
+        recordButton.textContent = 'Start Recording';
+        playButton.disabled = false;
+        downloadButton.disabled = false;
+    }
+});
+
+const playButton = document.querySelector('button#play');
+playButton.addEventListener('click', () => {
+    const superBuffer = new Blob(recordedBlobs, {type: mime});
+    recordedRemote.src = null;
+    recordedRemote.srcObject = null;
+    recordedRemote.src = window.URL.createObjectURL(superBuffer);
+    recordedRemote.controls = true;
+    recordedRemote.play();
+
+    recordedLocal.src = null;
+    recordedLocal.srcObject = null;
+    recordedLocal.src = window.URL.createObjectURL(superBuffer);
+    recordedLocal.controls = true;
+    recordedLocal.play();
+});
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+    // const blob = new Blob(recordedBlobs, {type: mime});
+    const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.webm';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 100);
+});
 
 const signalingWebsocket = new WebSocket("wss://" + window.location.host + "/socket");
 
@@ -39,7 +85,14 @@ signalingWebsocket.onmessage = function (msg) {
     }
 };
 
-// signalingWebsocket.onopen = init();
+signalingWebsocket.onopen = init({
+    audio: {
+        echoCancellation: {exact: true}
+    },
+    video: {
+        width: 1280, height: 720
+    }
+});
 
 function sendSignal(signal) {
     if (signalingWebsocket.readyState === 1) {
@@ -49,11 +102,10 @@ function sendSignal(signal) {
 
 async function init(constraints) {
     try {
-        // const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        // handleSuccess(stream);
         console.log("Connected to signaling endpoint. Now initializing.");
         preparePeerConnection();
-        displayLocalStreamAndSignal(true);
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream, true);
     } catch (e) {
         console.error('navigator.getUserMedia error:', e);
         errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
@@ -129,7 +181,7 @@ document.querySelector('button#start').addEventListener('click', async () => {
     document.querySelector('button#start').disabled = true;
     const constraints = {
         audio: {
-            // echoCancellation: {exact: true}
+            echoCancellation: {exact: true}
         },
         video: {
             width: 1280, height: 720
@@ -139,39 +191,18 @@ document.querySelector('button#start').addEventListener('click', async () => {
     await init(constraints);
 });
 
-function handleSuccess(stream) {
+function handleSuccess(stream, firstTime) {
     recordButton.disabled = false;
-    console.log('getUserMedia() got stream:', stream);
-    window.stream = stream;
-
-    const gumVideo = document.querySelector('video#localVideo');
-    gumVideo.srcObject = stream;
-
-    getSupportedMimeTypes().forEach(mimeType => {
-        const option = document.createElement('option');
-        option.value = mimeType;
-        option.innerText = option.value;
-        codecPreferences.appendChild(option);
-    });
-    codecPreferences.disabled = false;
-}
-
-async function displayLocalStreamAndSignal(firstTime) {
-    console.log('Requesting local stream');
     const localVideo = document.getElementById('localVideo');
-    let localStream;
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true
-        });
+        window.stream = stream;
         console.log('Received local stream');
         localVideo.srcObject = stream;
-        localStream = stream;
         if (firstTime) {
             setTimeout(
                 function () {
-                    addLocalStreamToPeerConnection(localStream);
+                    // addLocalStreamToPeerConnection(window.stream);
+                    addLocalStreamToPeerConnection(stream);
                 }, 2000);
         }
         sendOfferSignal();
@@ -180,4 +211,65 @@ async function displayLocalStreamAndSignal(firstTime) {
         throw e;
     }
     console.log('Start complete');
+}
+
+// async function displayLocalStreamAndSignal(firstTime) {
+//     console.log('Requesting local stream');
+//     const localVideo = document.getElementById('localVideo');
+//     let localStream;
+//     try {
+//         const stream = await navigator.mediaDevices.getUserMedia({
+//             audio: true,
+//             video: true
+//         });
+//         console.log('Received local stream');
+//         localVideo.srcObject = stream;
+//         localStream = stream;
+//         if (firstTime) {
+//             setTimeout(
+//                 function () {
+//                     addLocalStreamToPeerConnection(localStream);
+//                 }, 2000);
+//         }
+//         sendOfferSignal();
+//     } catch (e) {
+//         alert(`getUserMedia() error: ${e.name}\n` + e);
+//         throw e;
+//     }
+//     console.log('Start complete');
+// }
+
+function handleDataAvailable(event) {
+    console.log('handleDataAvailable', event);
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+function startRecording() {
+    recordedBlobs = [];
+    const options = {mime};
+    try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+        console.error('Exception while creating MediaRecorder:', e);
+        errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+        return;
+    }
+
+    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    recordButton.textContent = 'Stop Recording';
+    playButton.disabled = true;
+    downloadButton.disabled = true;
+    mediaRecorder.onstop = (event) => {
+        console.log('Recorder stopped: ', event);
+        console.log('Recorded Blobs: ', recordedBlobs);
+    };
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+    console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
 }
